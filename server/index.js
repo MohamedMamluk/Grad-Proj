b8679e8ff7b5906afe0e7def500a1f4dc772a57b
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-01-19 18:56:55 +0200

    ADD: Home route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to our project</h1>');
+});
+
 app.listen(7000, () => {
   console.log('server is running on port http://localhost:' + 7000);
 });
