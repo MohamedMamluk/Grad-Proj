53df81cebb1adbbb9c138fb001e429aed1291c76
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-02-18 11:31:02 +0200

    Add checkUserExists Middleware
    The function checks if the user wanting to register already exists in any of the models in the database and returns and error if the email already exists

---
+++ server/middleware/checkUserExists.js
@@ -0,0 +1,31 @@
+const { StatusCodes } = require('http-status-codes');
+
+const checkUserExists = (...models) => {
+  return async (req, res, next) => {
+    // Get the user data from the request body
+    const { email } = req.body;
+
+    // Loop through the models and check if the user exists
+    let userExists = false;
+    for (const Model of models) {
+      const user = await Model.findOne({ email });
+      //If user is found just exit from the loop
+      if (user) {
+        userExists = true;
+        break;
+      }
+    }
+
+    // If a user exists, return an error
+    if (userExists) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'User already exists.',
+      });
+    }
+
+    // Otherwise, call the next middleware
+    return next();
+  };
+};
+
+module.exports = checkUserExists;
