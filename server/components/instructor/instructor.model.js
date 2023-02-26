bc251b2a00b6dce1a37b37cd4db68afd8e726a96
ahmed-said-dev <eng.ahmed.said.97@gmail.com>
2023-02-26 09:19:13 +0200

    Added Instructor

---
+++ server/components/instructor/instructor.model.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const instructorSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "please provide your first name"],
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      required: [true, "please provide your last name"],
+      minLength: 3,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+    },
+    password: {
+      type: String,
+      minLength: [6, "Enter a password no less than 6 characters"],
+      required: [true, "Please provide an appropriate password"],
+    },
+    role: {
+      type: String,
+      required: [true, "Please tell us your role"],
+      default: "instructor",
+    },
+    phone: {
+      type: String,
+      required: [true, "Please provide a working number from your country"],
+    },
+    courses: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    levelOfExperience: {
+      type: String,
+      required: [true, "Please provide a level of experience"],
+    },
+  },
+  { timestamps: true }
+);
+
+instructorSchema.pre("save", async function () {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(this.password, salt);
+    this.password = hashed;
+  } catch (error) {
+    console.log(error);
+  }
+});
+instructorSchema.methods.comparePassword = async function (
+  USER_PASSWORD_FROM_FRONT
+) {
+  const isValid = bcrypt.compareSync(USER_PASSWORD_FROM_FRONT, this.password);
+  return isValid;
+};
+instructorSchema.methods.genJWT = function () {
+  const token = jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+  return token;
+};
+module.exports = mongoose.model("instructor", instructorSchema);
