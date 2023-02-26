114c8b4a70e37f04ca1baeff353a8e297157cf6b
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-02-26 18:03:08 +0200

    cleanup

--- server/components/auth/auth.controller.js
+++ server/components/auth/auth.controller.js
@@ -19,16 +19,22 @@ const login = async (req, res) => {
         .status(StatusCodes.NOT_FOUND)
         .json({ message: 'WRONG EMAIL OR PASSWORD' });
     }
-    console.log(user.password, req.body.password);
     const isValid = await user.comparePassword(req.body.password);
-    console.log(isValid);
     if (!isValid) {
       return res
         .status(StatusCodes.NOT_FOUND)
         .json({ message: 'WRONG EMAIL OR PASSWORD' });
     }
-    res.status(StatusCodes.OK).json(user);
-  } catch (error) {}
+    const token = user.genJWT();
+    //To be configured
+    // res.cookie('accessToken', token, {
+    //   maxAge: 3.154e10, // 1 year,
+    //   httpOnly: true, //can only be accessed with http requests and not js
+    // });
+    res.status(StatusCodes.OK).json({ token });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  }
 };
 
 module.exports = {
--- server/components/auth/auth.service.js
+++ server/components/auth/auth.service.js
@@ -1,38 +1,41 @@
-const AdminModel = require("../admin/admin.model");
-const StudentModel = require("../student/student.model");
-const InstructorModel = require("../instructor/instructor.model");
+const AdminModel = require('../admin/admin.model');
+const StudentModel = require('../student/student.model');
+const InstructorModel = require('../instructor/instructor.model');
 
 //Register user based on role, admin will be removed
 const registerService = async (USER) => {
   let user;
   switch (USER.role) {
-    case "admin":
+    case 'admin':
       user = await AdminModel.create(USER);
       return user;
-    case "student":
+    case 'student':
       user = await StudentModel.create(USER);
       return user;
-    case "instructor":
+    case 'instructor':
       user = await InstructorModel.create(USER);
       return user;
     default:
-      console.log("bara");
+      console.log('bara');
   }
 };
 
 const loginService = async (USER) => {
   switch (USER.role) {
-    case "admin":
-      let user = await AdminModel.findOne({ email: USER.email });
-      return user;
-    case "student":
-      user = await StudentModel.findOne({ email: USER.email });
-      return user;
-    case "instructor":
-      user = await InstructorModel.findOne({ email: USER.email });
-      return user;
+    case 'admin':
+      return AdminModel.findOne({ email: USER.email });
+    case 'student':
+      console.log('in Student service');
+      console.log(USER);
+
+      return StudentModel.findOne({ email: USER.email });
+    case 'instructor':
+      console.log('in instructor service');
+      console.log(USER);
+      return InstructorModel.findOne({ email: USER.email });
+
     default:
-      console.log("Please Register");
+      console.log('Please Register');
   }
 };
 module.exports = {
