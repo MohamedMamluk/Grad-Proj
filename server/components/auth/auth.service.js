3b8315fedf1335b5c60f3cfec1ee3fd4f1aef80d
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-02-26 18:17:15 +0200

    cleanup

--- server/components/auth/auth.service.js
+++ server/components/auth/auth.service.js
@@ -4,17 +4,13 @@ const InstructorModel = require('../instructor/instructor.model');
 
 //Register user based on role, admin will be removed
 const registerService = async (USER) => {
-  let user;
   switch (USER.role) {
     case 'admin':
-      user = await AdminModel.create(USER);
-      return user;
+      return await AdminModel.create(USER);
     case 'student':
-      user = await StudentModel.create(USER);
-      return user;
+      return await StudentModel.create(USER);
     case 'instructor':
-      user = await InstructorModel.create(USER);
-      return user;
+      return await InstructorModel.create(USER);
     default:
       console.log('bara');
   }
@@ -25,13 +21,8 @@ const loginService = async (USER) => {
     case 'admin':
       return AdminModel.findOne({ email: USER.email });
     case 'student':
-      console.log('in Student service');
-      console.log(USER);
-
       return StudentModel.findOne({ email: USER.email });
     case 'instructor':
-      console.log('in instructor service');
-      console.log(USER);
       return InstructorModel.findOne({ email: USER.email });
 
     default:
