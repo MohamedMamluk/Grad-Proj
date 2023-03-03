3813dd179f55d1ceba797d46d79af484a06b1871
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-03 10:03:15 +0200

    Add AllCourseComponent

--- client/src/components/CoursesHeader/CouHeader.jsx
+++ client/src/components/CoursesHeader/CouHeader.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./CouHeader.css";
+import React from 'react';
+import './CouHeader.css';
 import { Link } from 'react-router-dom';
 const CouHeader = () => {
   return (
     <div>
-      <div className=" container-fluid " id="Nav_Div">
+      <div className=' container mt-2 ' id='Nav_Div'>
         <div>
           <div>
-            <h3 id="Title">Courses Section</h3>
+            <h3 id='Title'>Courses Section</h3>
           </div>
-          <div id="preDiv">
+          <div id='preDiv'>
             <p>
               This Section contains courses from different directions that are
               available at our website
@@ -17,21 +17,23 @@ const CouHeader = () => {
           </div>
         </div>
 
-        <div id="imgDiv">
-          <img src="Images/girl.png"></img>
+        <div id='imgDiv'>
+          <img src='/girl.png' />
         </div>
       </div>
 
-      <div className=" container " id="btnDiv">
+      <div className=' container ' id='btnDiv'>
         {/* <Link to='/AllCourses'>All</Link> */}
-        
-        <label for="Categories"> Categories :</label>
-        <select name="Categories" id="Categories">
-         <option value="ALL">All</option>
-          <option value="Web Development ">Web Development</option>
-          <option value=" Mobile Development ">Mobile Development </option>
-          <option value="Artificial intelligence">Artificial intelligence</option>
-          <option value="Business">Business</option>
+
+        <label htmlFor='Categories'> Categories :</label>
+        <select name='Categories' id='Categories'>
+          <option value='ALL'>All</option>
+          <option value='Web Development '>Web Development</option>
+          <option value=' Mobile Development '>Mobile Development </option>
+          <option value='Artificial intelligence'>
+            Artificial intelligence
+          </option>
+          <option value='Business'>Business</option>
         </select>
       </div>
     </div>
