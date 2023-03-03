5edfc9f486931ba8f2e85f0c9b0cc77ae5538f80
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-03 10:13:47 +0200

    Add home component and dashboard component

--- client/src/pages/Dashboard/Home.jsx
+++ client/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUserData } from '../../features/auth/authSlice';
 const DashboardHome = () => {
-  return <div>DashboardHome</div>;
+  const user = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const id = localStorage.getItem('id');
+    const role = localStorage.getItem('role');
+
+    axios
+      .get(`http://localhost:7000/api/${user.role || role}/${user.id || id}`)
+      .then((res) => {
+        dispatch(setUserData(res.data.user));
+        console.log(user);
+      });
+  }, []);
+  if (!user.userData) {
+    return <h1>loading...</h1>;
+  }
+  return <div>Hello, {user.userData.email}</div>;
 };
 
 export default DashboardHome;
