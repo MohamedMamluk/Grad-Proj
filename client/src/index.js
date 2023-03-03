6c4c5eceaae64a14989fe4b37129fda0b1ee92ad
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-03 10:20:34 +0200

    Adjust sidenav icons

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider, useSelector } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import Header from "./components/header/header"
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Header from './components/header/header';
+import { setUser } from './features/auth/authSlice';
 const container = document.getElementById('root');
 const root = createRoot(container);
 function Index() {
   const user = useSelector((store) => store.auth.token);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const id = localStorage.getItem('id');
+    const role = localStorage.getItem('role');
+    if (token) {
+      dispatch(setUser({ token, id, role }));
+    }
+  }, []);
   return (
     <>
       <Router>
-
         {/* <header>
           <Link to='/'>Home</Link>
           <Link to='/login'>login</Link>
@@ -21,7 +31,6 @@ function Index() {
           <Link to='/courses'>Courses</Link>
           <Link to='/dashboard'>Dashboard</Link>
         </header> */}
-        <Header></Header>
 
         <App />
       </Router>
--- client/src/pages/Dashboard/listItems.js
+++ client/src/pages/Dashboard/listItems.js
@@ -4,67 +4,75 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ExploreIcon from '@mui/icons-material/Explore';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../features/auth/authSlice';
 
-export const mainListItems = (
-  <React.Fragment>
-    <Link to='/dashboard' style={{ textDecoration: 'none', color: 'GrayText' }}>
-      <ListItemButton>
-        <ListItemIcon>
-          <DashboardIcon style={{ fill: '#6d54de' }} fontSize='large' />
-        </ListItemIcon>
-        <ListItemText primary='Main' />
-      </ListItemButton>
-    </Link>
-    <Link
-      to='/dashboard/explore'
-      style={{ textDecoration: 'none', color: 'GrayText' }}
-    >
-      <ListItemButton>
-        <ListItemIcon>
-          <ExploreIcon
-            style={{ fill: '#6d54de', textAlign: 'center' }}
-            fontSize='large'
-          />
-        </ListItemIcon>
-        <ListItemText primary='Explore' />
-      </ListItemButton>
-    </Link>
-    <Link
-      to='/dashboard/profile'
-      style={{ textDecoration: 'none', color: 'GrayText' }}
-    >
-      <ListItemButton>
-        <ListItemIcon>
-          <AccountCircleIcon style={{ fill: '#6d54de' }} fontSize='large' />
-        </ListItemIcon>
-        <ListItemText primary='Profile' />
-      </ListItemButton>
-    </Link>
-    <Link to='/' style={{ textDecoration: 'none', color: 'GrayText' }}>
-      <ListItemButton>
-        <ListItemIcon>
-          <LogoutIcon style={{ fill: '#6d54de' }} fontSize='large' />
-        </ListItemIcon>
-        <ListItemText primary='Sign Out' />
-      </ListItemButton>
-    </Link>
-    {/* <ListItemButton>
+export const MainListItems = () => {
+  const dispatch = useDispatch();
+  return (
+    <React.Fragment>
+      <Link
+        to='/dashboard'
+        style={{ textDecoration: 'none', color: 'GrayText' }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <DashboardIcon style={{ fill: '#6d54de' }} fontSize='large' />
+          </ListItemIcon>
+          <ListItemText primary='Main' />
+        </ListItemButton>
+      </Link>
+      <Link
+        to='/dashboard/explore'
+        style={{ textDecoration: 'none', color: 'GrayText' }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <ExploreIcon
+              style={{ fill: '#6d54de', textAlign: 'center' }}
+              fontSize='large'
+            />
+          </ListItemIcon>
+          <ListItemText primary='Explore' />
+        </ListItemButton>
+      </Link>
+      <Link
+        to='/dashboard/profile'
+        style={{ textDecoration: 'none', color: 'GrayText' }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <AccountCircleIcon style={{ fill: '#6d54de' }} fontSize='large' />
+          </ListItemIcon>
+          <ListItemText primary='Profile' />
+        </ListItemButton>
+      </Link>
+      <Link
+        to='/'
+        onClick={() => dispatch(logout())}
+        style={{ textDecoration: 'none', color: 'GrayText' }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <LogoutIcon style={{ fill: '#6d54de' }} fontSize='large' />
+          </ListItemIcon>
+          <ListItemText primary='Sign Out' />
+        </ListItemButton>
+      </Link>
+      {/* <ListItemButton>
       <ListItemIcon>
         <LayersIcon />
       </ListItemIcon>
       <ListItemText primary='Integrations' />
     </ListItemButton> */}
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
+};
 
 export const secondaryListItems = (
   <React.Fragment>
