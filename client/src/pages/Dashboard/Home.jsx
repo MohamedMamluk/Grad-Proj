b9f1a60865b8eee59493221eeeccf645c0add396
mariamhus <mariamhus2000@gmail.com>
2023-03-04 21:32:22 +0200

    learning progress / home header components

--- client/src/pages/Dashboard/Home.jsx
+++ client/src/pages/Dashboard/Home.jsx
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { setUserData } from '../../features/auth/authSlice';
-import { Box, Container, Typography } from '@mui/material';
+import HomeHeader from './components/homeComponents/HomeHeader';
+import { Container } from '@mui/material';
+import LearningProgress from './components/homeComponents/LearningProgress';
+
 const DashboardHome = () => {
   const user = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -21,49 +24,9 @@ const DashboardHome = () => {
     return <h1>loading...</h1>;
   }
   return (
-    <Container
-      className='hello-section'
-      sx={{
-        borderRadius: '20px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        paddingTop: '10px',
-      }}
-    >
-      <Box className='left-box'>
-        <h2 className='home-welcome'>
-          Greetings, {user.userData.firstName || user.userData.email}
-        </h2>
-        <Box>
-          <h6 className='home-introduction'>
-            Welcome to MindsOn! Study with us.
-          </h6>
-          <h6 className='home-introduction'>
-            At any time and in any place, discover the unknown.
-          </h6>{' '}
-          <h6 className='home-introduction'>
-            On the main page there are elements on time management
-          </h6>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          width: '35%',
-          height: '200px',
-        }}
-        className='d-none d-md-block'
-      >
-        <img
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'contain',
-            objectPosition: 'left',
-          }}
-          src='/homeImg.png'
-          alt=''
-        />
-      </Box>
+    <Container>
+      <HomeHeader user={user}/>
+      <LearningProgress/>
     </Container>
   );
 };
