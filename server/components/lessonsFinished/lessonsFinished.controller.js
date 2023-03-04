7ae53ddc93df8d5262dc5538edeeb28c7880265c
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-04 14:06:51 +0200

    Co-authored-by: ReemElhadek <ReemElhadek@users.noreply.github.com> Co-authored-by: mariamhus2000@gmail.com <mariamhus2000@gmail.com>

---
+++ server/components/lessonsFinished/lessonsFinished.controller.js
@@ -0,0 +1,16 @@
+const getAllLessonsFinished = async (req, res) => {};
+const getOneLessonFinished = async (req, res) => {};
+
+const createLessonsFinished = async (req, res) => {};
+
+const updateLessonFinished = async (req, res) => {};
+
+const deleteLessonFinished = async (req, res) => {};
+
+module.exports = {
+  getAllLessonsFinished,
+  getOneLessonFinished,
+  createLessonsFinished,
+  updateLessonFinished,
+  deleteLessonFinished,
+};
