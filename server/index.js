20245ce569de55643793cd2b27cf42518de57fc6
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-04 14:07:23 +0200

    add routes

--- server/index.js
+++ server/index.js
@@ -7,8 +7,11 @@ const authRoutes = require('./components/auth/auth.route');
 const lessonRoutes = require('./components/lesson/lesson.routes');
 const courseInfoRoute = require('./components/courseInfo/courseInfo.route');
 const StudentRoutes = require('./components/student/StudentsRoutes');
+const lessonsFinishedRoutes = require('./components/lessonsFinished/lessonFinished.routes');
 const instructorRoutes = require('./components/instructor/instructor.route');
+const enrollmentRoutes = require('./components/enrollment/enrollment.routes');
 const AdminModel = require('./components/admin/admin.model');
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 var cors = require('cors');
 app.use(cors());
 app.use(express.json());
@@ -17,8 +20,35 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to our project</h1>');
 });
+// app.get('/config', (req, res) => {
+//   res.send({
+//     publishableKey: process.env.STRIPE_PUBLIC_KEY,
+//   });
+// });
+// app.post('/create-payment-intent', async (req, res) => {
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       currency: 'EUR',
+//       amount: req.body.price,
+//       automatic_payment_methods: { enabled: true },
+//     });
+
+//     // Send publishable key and PaymentIntent details to client
+//     res.send({
+//       clientSecret: paymentIntent.client_secret,
+//     });
+//   } catch (e) {
+//     return res.status(400).send({
+//       error: {
+//         message: e.message,
+//       },
+//     });
+//   }
+// });
+
 //auth route
 app.use('/api/auth', authRoutes);
+app.use('/api/enrollment', enrollmentRoutes);
 app.get('/api/admin/:id', async (req, res) => {
   try {
     const user = await AdminModel.findById(req.params.id);
@@ -34,6 +64,7 @@ app.use('/api/lesson', lessonRoutes);
 //courseInfo  route
 app.use('/api/courseinfo', courseInfoRoute);
 app.use('/api/student', StudentRoutes);
+app.use('/api/student/:id/lessonsFinished', lessonsFinishedRoutes);
 app.use('/api/instructor', instructorRoutes);
 app.use('/api/course', courseRoutes);
 
