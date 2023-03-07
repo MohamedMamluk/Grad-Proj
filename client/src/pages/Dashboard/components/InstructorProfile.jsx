dd726467796862089db2bb323e122556aa5f677a
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2023-03-07 15:52:52 +0200

    Add updateProfile

--- client/src/pages/Dashboard/components/InstructorProfile.jsx
+++ client/src/pages/Dashboard/components/InstructorProfile.jsx
@@ -9,165 +9,253 @@ import Chip from '@mui/material/Chip';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import { NavLink } from 'react-router-dom';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Pagination from '@mui/material/Pagination';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { useDispatch } from 'react-redux';
+import { Button, Grid, TextField } from '@mui/material';
+import axios from 'axios';
+import { setUserData } from '../../../features/auth/authSlice';
 
 const InstructorProfile = ({ userData }) => {
-  const breadcrumbs = [
-    <NavLink underline='hover' color='inherit' to={'/dashboard'}>
-      My Dashboard
-    </NavLink>,
-    <Typography key='3' color='text.primary'>
-      Profile
-    </Typography>,
-  ];
-  const [showSkeleton, setShowSkeleton] = useState(true);
+  const dispatch = useDispatch();
+  const [uploading, setUploading] = useState(false);
+  const [uploaded, setUploaded] = useState('');
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSkeleton(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setIsVisible(true);
-    }, 2000);
-
-    return () => clearTimeout(delay);
-  }, []);
+  const [showUpdateProfile, setShowUpdateProfile] = useState(false);
+  const [updateData, setUpdateData] = useState(userData);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'vqsktgr7');
+    setUploading(true);
+    axios
+      .post('https://api.cloudinary.com/v1_1/dsra1ldsf/image/upload', formData)
+      .then((res) => {
+        setUploading(false);
+        // //console.log(res.data);
+        setUploaded(res.data.public_id);
+        setUpdateData((prev) => {
+          return { ...prev, image: res.data.secure_url };
+        });
+      });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.patch(
+        '/instructor/' + userData._id,
+        updateData
+      );
+      dispatch(setUserData(data));
+      setShowUpdateProfile(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      {showSkeleton && <Skeleton variant='circular' width={40} height={40} />}
-      {showSkeleton && <Skeleton />}
-      {showSkeleton && <Skeleton />}
-      {showSkeleton && <Skeleton /> && (
-        <Skeleton variant='text' sx={{ fontSize: '1rem' }} />
-      )}
-      {showSkeleton && <Skeleton variant='rounded' width={210} height={60} />}
       <div>
-        {isVisible && (
-          <div>
-            <Alert
-              iconMapping={{
-                success: <CheckCircleOutlineIcon fontSize='inherit' />,
-              }}
-            >
-              Your profile is verified as a instructor profile !
-            </Alert>
+        <div>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color='text.secondary'
+                gutterBottom
+              ></Typography>
 
-            <Card sx={{ minWidth: 275 }}>
-              <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color='text.secondary'
-                  gutterBottom
-                ></Typography>
-
-                <Box
-                  sx={{
-                    mx: 'auto',
-                    width: 250,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Box sx={{ mx: 'auto', width: 150 }}>
-                    <Stack direction='row' spacing={1} align='center'>
+              <Box
+                sx={{
+                  mx: 'auto',
+                  width: 250,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Box sx={{ mx: 'auto', width: 150 }}>
+                  <Stack direction='row' spacing={1} align='center'>
+                    {userData.image ? (
+                      <Avatar
+                        src={userData.image}
+                        sx={{ width: 80, height: 80, mx: 'auto' }}
+                      />
+                    ) : (
                       <Avatar
                         src='/broken-image.jpg'
                         sx={{ width: 80, height: 80, mx: 'auto' }}
                       />
-                    </Stack>
-                  </Box>
-                  <Typography
-                    variant='h5'
-                    component='div'
-                    align='center'
-                    sx={{ mb: 1.5, mt: 1.5 }}
-                  >
-                    {userData.firstName} {userData.lastName}
-                  </Typography>
-                  <Typography
-                    sx={{ mb: 1.5, mt: 1 }}
-                    color='text.secondary'
-                    align='center'
-                  >
-                    <Chip label={userData.role} />
-                  </Typography>
-                  <Typography
-                    sx={{ mb: 1.5 }}
-                    color='text.secondary'
-                    align='center'
-                  >
-                    <Chip
-                      label={userData.email}
-                      variant='outlined'
-                      align='center'
-                    />
-                  </Typography>
-                  <Typography variant='body2' align='center'>
-                    {'We hope you enjoy teaching your courses'}
-                  </Typography>
-                  <Stack
-                    direction='row'
-                    sx={{ mt: 1.5, mx: 7.2, width: 150 }}
-                    align='center'
-                  >
-                    <Chip
-                      label='Courses Taught :'
-                      color='primary'
-                      variant='outlined'
-                      align='center'
-                    />
+                    )}
                   </Stack>
                 </Box>
-              </CardContent>
-            </Card>
+                <Typography
+                  variant='h5'
+                  component='div'
+                  align='center'
+                  sx={{ mb: 1.5, mt: 1.5 }}
+                >
+                  {userData.firstName} {userData.lastName}
+                </Typography>
+                <Typography
+                  sx={{ mb: 1.5, mt: 1 }}
+                  color='text.secondary'
+                  align='center'
+                >
+                  <Chip label={userData.role} />
+                </Typography>
+                <Typography
+                  sx={{ mb: 1.5 }}
+                  color='text.secondary'
+                  align='center'
+                >
+                  <Chip
+                    label={userData.email}
+                    variant='outlined'
+                    align='center'
+                  />
+                </Typography>
 
-            <ul>
-              <div id='coursesContainer' className='container'>
-                {userData.courses.map((course) => (
-                  <div key={course._id} className='card p-3 m-2 '>
-                    <div id='card-image__container'>
-                      <img src={course.image} id='card-image' alt='...' />
-                    </div>
-                    <div className='card-body'>
-                      <p className='card-text' title={course.name}>
-                        {course.name.length > 50
-                          ? course.name.substring(0, 45) + '...'
-                          : course.name}
-                      </p>
-                      <p className='card-text'>{course.cost}</p>
-                      <p className='card-text'>{course.duration}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </ul>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                p: 1,
-                m: 1,
-                bgcolor: 'background.paper',
-                borderRadius: 1,
+                <Stack
+                  direction='row'
+                  sx={{ mt: 1.5, mx: 7.2, width: 250 }}
+                  align='center'
+                >
+                  <Button
+                    variant='outlined'
+                    fullWidth
+                    onClick={() => setShowUpdateProfile(!showUpdateProfile)}
+                    sx={{ width: 'max-content', wordBreak: 'keep-all' }}
+                  >
+                    Update Profile
+                  </Button>
+                </Stack>
+              </Box>
+            </CardContent>
+          </Card>
+          <Grid
+            component='form'
+            onSubmit={handleSubmit}
+            sx={{
+              display: `${showUpdateProfile ? 'block' : 'none'}`,
+            }}
+          >
+            <Grid item xs={12}>
+              <Typography variant='h6' gutterBottom textAlign='center'>
+                Update User Picture
+              </Typography>
+              <Grid display='flex' justifyContent='center' alignItems='center'>
+                <Stack direction='row' alignItems='center' spacing={2}>
+                  <Button
+                    variant='contained'
+                    component='label'
+                    disabled={uploading || uploaded.length > 0}
+                    sx={{
+                      backgroundColor:
+                        uploading || uploaded.length > 0 ? 'gray' : '#3f51b5',
+                      color: uploading || uploaded.length > 0 ? 'red' : 'white',
+                    }}
+                  >
+                    {uploading && 'Uploading...'}
+                    {uploaded.length > 0 ? 'Uploaded ✔' : 'Upload'}
+                    <input
+                      hidden
+                      accept='image/*'
+                      multiple
+                      onChange={handleFileChange}
+                      type='file'
+                    />
+                  </Button>
+                </Stack>
+              </Grid>
+            </Grid>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='firstName'
+              value={updateData.firstName}
+              onChange={(e) => {
+                setUpdateData((prev) => {
+                  return { ...prev, firstName: e.target.value };
+                });
               }}
+              label='First Name'
+              name='firstName'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='lastName'
+              value={updateData.lastName}
+              onChange={(e) => {
+                setUpdateData((prev) => {
+                  return { ...prev, lastName: e.target.value };
+                });
+              }}
+              label='Last Name'
+              name='lastName'
+              autoFocus
+            />
+
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              value={updateData.email}
+              onChange={(e) => {
+                setUpdateData((prev) => {
+                  return { ...prev, email: e.target.value };
+                });
+              }}
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='phone'
+              value={updateData.phone}
+              onChange={(e) => {
+                setUpdateData((prev) => {
+                  return { ...prev, phone: e.target.value };
+                });
+              }}
+              label='Phone'
+              name='phone'
+              autoComplete='phone'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='levelOfExperience'
+              value={updateData.levelOfExperience}
+              onChange={(e) => {
+                setUpdateData((prev) => {
+                  return { ...prev, levelOfExperience: e.target.value };
+                });
+              }}
+              label='Level Of Experience'
+              name='levelOfExperience'
+              autoComplete='levelOfExperience'
+              autoFocus
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              style={{ backgroundColor: '#3f51b5' }}
+              sx={{ mt: 3, mb: 2 }}
             >
-              <Stack spacing={1}>
-                <Pagination count={5} disabled />
-              </Stack>
-            </Box>
-          </div>
-        )}
+              Update
+            </Button>
+          </Grid>
+        </div>
       </div>
     </div>
   );
