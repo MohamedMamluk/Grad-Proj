b4315be0a94c47fb637239f6f7bdd976781bbf20
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-03-20 17:21:39 +0200

    change placeholder for email and add button

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
               type='email'
               id='email'
               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='name@flowbite.com'
+              placeholder='name@provider.com'
               required
             />
           </div>
@@ -60,7 +60,7 @@ const Contact = () => {
           </div>
           <button
             type='submit'
-            className='py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+            className='py-3 px-5 text-sm font-medium text-center text-white rounded-lg !bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
           >
             Send message
           </button>
