50c94c14a9d084fcb8f1a403a31be1e31e4204dc
mariamhus <mariamhus2000@gmail.com>
2023-03-20 12:53:29 +0200

    dashboard animation

--- client/src/pages/Dashboard/pages/home-page/Home.jsx
+++ client/src/pages/Dashboard/pages/home-page/Home.jsx
@@ -12,18 +12,14 @@ import TheTOdo from '../../components/to-do-list/TheTOdo';
 import Video from '../../../../components/lessonTypes/Video';
 import LineChart from '../../../../components/charts/LineChart';
 import Loader from '../../../../components/loading/loading';
-import { motion, AnimatePresence, useAnimation} from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const DashboardHome = () => {
   const user = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const {ref , inView} = useInView({
-    threshold:0.1
-    //that means 10 of the paent div should be visible before it sets the boolean inView to true
-  });
-  const animation =useAnimation();
-
+  const {ref , inView} = useInView();
+  
   useEffect(() => {
     const id = localStorage.getItem('id');
     const role = localStorage.getItem('role');
@@ -32,21 +28,6 @@ const DashboardHome = () => {
       dispatch(setUserData(res.data.user));
     });
   }, []);
-
-  useEffect(()=>{
-    if(inView){
-      animation.start({
-        x:0,
-        transition:{
-          type:'spring',duration:1, bounce:0.3
-        }
-      });
-    }
-    if(!inView){
-      animation.start({x:'-100vw'})
-    }
-  },[inView]);
-
   if (!user.userData) {
     return (<div style={{position: 'absolute', left: '10%', top: '50%'}}>
     <Loader/>
@@ -55,20 +36,22 @@ const DashboardHome = () => {
 
   return (
     <div>
-      <HomeHeader user={user} />
+      <motion.div id='progress_container'
+          initial={{x:'-100vw'}}
+          animate={{x:0}}
+          transition={{type:'spring',duration:1,bounce:0.3}}>
+            <HomeHeader user={user} />
+            </motion.div>
       <Grid ref={ref} container spacing={4} padding={5}>
         {user.role == 'instructor' && user.userData.balance.length > 0 && (
-          <motion.div id='balance_container'
-          animate={{animation}}
-          >
-            <LineChart />
-            </motion.div>
+          <LineChart />
         )}
 
         {user.role == 'student' && (
           <motion.div id='progress_container'
-          animate={{animation}}
-          >
+          initial={{x:'-100vw'}}
+          animate={{x:0}}
+          transition={{type:'spring',duration:2,bounce:0.3}}>
             <LearningProgress />
             <CourseProgress />
           </motion.div>
@@ -80,11 +63,12 @@ const DashboardHome = () => {
           </div>
         )}
       </Grid>
-      <motion.div 
-      animate={{animation}}
-          >
+      <motion.div id='progress_container'
+          initial={{x:'-100vw'}}
+          animate={{x:0}}
+          transition={{type:'spring',duration:3,bounce:0.3}}>
         <TheTOdo />
-      </motion.div>
+            </motion.div>
     </div>
   );
 };
